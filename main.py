a5f1e41b59e2db73fdaa4bc4d29643574a44eddc
Sajan Kumar Kar <sajan.kumar.kar@gmail.com>
2024-04-27 19:55:33 -0400

    SFM/Sparse Reconstruction achieved

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from scripts.match import visualize_matches
 from pathlib import Path
 import pycolmap
 import torch
+import pycolmap
+from hloc.utils import viz_3d
+
 
 PATH = '/home/ubuntu/DepthArt/train/haiper/bike/images'
 EXT = 'jpeg'
@@ -15,7 +18,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Get Image Pairs
 images_list = list(Path(PATH).glob(f'*.{EXT}'))[:10]
-index_pairs = get_image_pairs(images_list, DINO_PATH) #TODO: Add Dinov2 path
+index_pairs = get_image_pairs(images_list, DINO_PATH)
 
 # Extract keypoints
 feature_dir = Path(PATH_FEATURES)
@@ -52,3 +55,16 @@ maps = pycolmap.incremental_mapping(
     options=mapper_options,
 )
 
+
+unidstorted_outputs = Path.cwd() / "unidstorted_outputs"
+fusion_outputs = Path.cwd() / "stereofusion_outputs"
+sparse_outputs = Path.cwd() / 'sparse_outputs'
+workspace_path = Path.cwd() / 'workspace'
+
+# Run sparse reconstruction
+rec_gt = pycolmap.Reconstruction(f'{Path.cwd()}/incremental_pipeline_outputs/0')
+fig = viz_3d.init_figure()
+viz_3d.plot_cameras(fig, rec_gt, color='rgba(50,255,50, 0.5)', name="Ground Truth", size=10)
+viz_3d.plot_reconstruction(fig, rec_gt, cameras = False, color='rgba(255,50,255, 0.5)', name="Ground Truth", cs=5)
+fig.write_html('Sparse_Reconstruction.html')
+# fig.show()
