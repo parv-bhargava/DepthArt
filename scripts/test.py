806b8cb1e4776f47c26027fc29c9c755334ca060
Tanmay1005 <ambegaokart@gmail.com>
2024-04-29 18:07:22 -0400

    changed test file to show extracted keypoints

--- scripts/test.py
+++ scripts/test.py
@@ -1,30 +1,32 @@
-from image_pair import get_image_pairs
-from utils import *
+# from image_pair import get_image_pairs
+from scripts.utils import *
+import streamlit as st
+# images_list = list(Path("/home/ubuntu/DepthArt/train/haiper/bike/images").glob("*.jpeg"))[:10]
+# index_pairs = get_image_pairs(images_list, "/home/ubuntu/DepthArt/dinov2/pytorch/base/1")
+# print(index_pairs)
+def points(images_list):
+    if True:
+        dtype = torch.float32  # ALIKED has issues with float16
 
-images_list = list(Path("/home/ubuntu/DepthArt/train/haiper/bike/images").glob("*.jpeg"))[:10]
-index_pairs = get_image_pairs(images_list, "/home/ubuntu/DepthArt/dinov2/pytorch/base/1")
-print(index_pairs)
+        extractor = ALIKED(
+            max_num_keypoints=4096,
+            detection_threshold=0.01,
+            resize=1024
+        ).eval().to(device, dtype)
 
+        path = images_list[0]
+        image = load_torch_image(path, device=device).to(dtype)
+        features = extractor.extract(image)
+
+        fig, ax = plt.subplots(1, 2, figsize=(10, 20))
+        ax[0].imshow(image[0, ...].permute(1, 2, 0).cpu())
+        ax[1].imshow(image[0, ...].permute(1, 2, 0).cpu())
+        ax[1].scatter(features["keypoints"][0, :, 0].cpu(), features["keypoints"][0, :, 1].cpu(), s=0.5, c="red")
+        plt.show()
+        st.pyplot(fig)
+        del extractor
 # Extract keypoints
-if True:
-    dtype = torch.float32  # ALIKED has issues with float16
 
-    extractor = ALIKED(
-        max_num_keypoints=4096,
-        detection_threshold=0.01,
-        resize=1024
-    ).eval().to(device, dtype)
-
-    path = images_list[0]
-    image = load_torch_image(path, device=device).to(dtype)
-    features = extractor.extract(image)
-
-    fig, ax = plt.subplots(1, 2, figsize=(10, 20))
-    ax[0].imshow(image[0, ...].permute(1, 2, 0).cpu())
-    ax[1].imshow(image[0, ...].permute(1, 2, 0).cpu())
-    ax[1].scatter(features["keypoints"][0, :, 0].cpu(), features["keypoints"][0, :, 1].cpu(), s=0.5, c="red")
-    plt.show()
-    del extractor
 # # Compute Keypoint Distances
 # feature_dir = Path("/home/ubuntu/DepthArt/features")
 # # keypoint_distances(images_list, index_pairs, feature_dir, verbose=False)
